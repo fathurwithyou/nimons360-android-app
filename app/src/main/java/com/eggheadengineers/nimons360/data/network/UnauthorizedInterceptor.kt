69ac9993649e69fcf11816aa194384a90faa603e
KarRz <reza11781@gmail.com>
2026-04-18 10:38:43 +0700

    fix(interceptor): correct response code check for unauthorized access

--- app/src/main/java/com/eggheadengineers/nimons360/data/network/UnauthorizedInterceptor.kt
+++ app/src/main/java/com/eggheadengineers/nimons360/data/network/UnauthorizedInterceptor.kt
@@ -7,7 +7,7 @@ import okhttp3.Response
 class UnauthorizedInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val response = chain.proceed(chain.request())
-        if (response.code == 409) {
+        if (response.code == 401) {
             UnauthorizedEvent.emit()
         }
         return response
